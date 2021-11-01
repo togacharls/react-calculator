6ded39499432efa13c0e5e2b813042fff0cf926e
Carlos Torres García <togacharls@gmail.com>
2021-11-01 14:03:34 +0100

    feat: overflow prevented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import CalculatorScreen from './CalculatorScreen.js';
 import CalculatorKeyboard from './CalculatorKeyboard.js';
 import React, { useState } from 'react';
 
+const OVERFLOW = 'Overflow';
+const MAX_RESULT_LENGTH = 14;
+const MAX_NUMBER = Number('9'.repeat(MAX_RESULT_LENGTH));
 const ZERO = '0';
 const DECIMAL = ',';
 const DECIMAL_DOT = '.';
@@ -29,7 +32,12 @@ function App() {
   ];
 
   const calc = () => {
-    let operator = '', result, operationToNumber, resultToNumber;
+    let operator = '',
+      operationResult,
+      operationToNumber,
+      resultToNumber,
+      result,
+      decimalIndex;
     const thereIsOperator = operations.some(op => {
       if (state.operation.includes(op)) {
         operator = op;
@@ -42,53 +50,73 @@ function App() {
       resultToNumber = state.result.replace(DECIMAL, DECIMAL_DOT);
       switch (operator) {
         case SUM:
-          result = (Number(operationToNumber) + Number(resultToNumber));
+          operationResult = (Number(operationToNumber) + Number(resultToNumber));
           break;
         case SUBSTRACTION:
-          result = (Number(operationToNumber) - Number(resultToNumber));
+          operationResult = (Number(operationToNumber) - Number(resultToNumber));
           break;
         case MULTIPLICATION:
-          result = (Number(operationToNumber) * Number(resultToNumber));
+          operationResult = (Number(operationToNumber) * Number(resultToNumber));
           break;
         case DIVISION:
-          result = (Number(operationToNumber) / Number(resultToNumber));
+          operationResult = (Number(operationToNumber) / Number(resultToNumber));
           break;
         case MODULE:
-          result = (Number(operationToNumber) % Number(resultToNumber));
+          operationResult = (Number(operationToNumber) % Number(resultToNumber));
           break;
       }
-      setState({ operation: '', result: result.toString().replace(DECIMAL_DOT, DECIMAL) });
+
+      result = operationResult.toString().replace(DECIMAL_DOT, DECIMAL);
+      decimalIndex = result.indexOf(DECIMAL);
+
+      if (operationResult > MAX_NUMBER) {
+        result = OVERFLOW;
+      } else if ( decimalIndex > -1 && result.length > MAX_RESULT_LENGTH) {
+        result = result.substr(0, decimalIndex <= (MAX_RESULT_LENGTH -2) ? MAX_RESULT_LENGTH : decimalIndex);
+      }
+
+      setState({ operation: '', result });
     }
   }
 
   const onClickRemover = (button) => {
-    switch(button) {
-      case removers[0]:
-        setState({ operation: '', result: ZERO });
-        break;
-      case removers[1]:
-        setState({ ...state, result: ZERO });
-        break;
-      case removers[2]:
-        setState({ ...state, result: state.result.slice(0, -1) || ZERO });
-        break;
+    if (state.result !== OVERFLOW) {
+      switch(button) {
+        case removers[0]:
+          setState({ operation: '', result: ZERO });
+          break;
+        case removers[1]:
+          setState({ ...state, result: ZERO });
+          break;
+        case removers[2]:
+          setState({ ...state, result: state.result.slice(0, -1) || ZERO });
+          break;
+      }
     }
   }
 
   const onClickOperation = (button) => {
     if (operations.some(operator => state.operation.includes(operator))) {
       setState({ ...state, operation: state.operation.slice(0, -1) + button });
+    } else if (state.result === OVERFLOW) {
+      setState({ operation: ZERO + button, result: ZERO });
     } else if (state.result.substr(-1) !== DECIMAL) {
       setState({ operation: state.result + button, result: ZERO });
     }
   }
 
   const onClickNumber = (button) => {
-    setState({ ...state, result: state.result === ZERO ? button : state.result + button });
+    if (state.result === OVERFLOW) {
+      setState({ ...state, result: button });
+    } else if (state.result.length < MAX_RESULT_LENGTH) {
+      setState({ ...state, result: state.result === ZERO ? button : state.result + button });
+    }
   }
 
   const onClickDecimal = () => {
-    if (!isNaN(Number(state.result))) {
+    if (state.result === OVERFLOW) {
+      setState({ ...state, result: ZERO + DECIMAL });
+    } else if (!isNaN(Number(state.result))) {
       setState({ ...state, result: state.result + DECIMAL });
     }
   }
